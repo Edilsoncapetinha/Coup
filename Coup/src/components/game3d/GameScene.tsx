2f629f2c54e5ef304259f0899999594ae8bcec08
Edilsoncapetinha <f27bertolin@aluno.ufsj.edu.br>
2026-02-21 16:43:22 -0300

    style(3d): premium Speakeasy overhaul with procedural textures and Noir lighting

--- Coup/src/components/game3d/GameScene.tsx
+++ Coup/src/components/game3d/GameScene.tsx
@@ -6,7 +6,7 @@ import PlayerModel from './PlayerModel';
 import Card3D from './Card3D';
 import CoinPile from './CoinPile';
 import { getPlayerPositions, getCardPositions, getCoinPosition, getHumanCameraPos } from './TablePositions';
-import { Bottle, Book, Barrel, WallSconce } from './Props';
+import { Bottle, Book, Barrel, WallSconce, woodTex, brickTex } from './Props';
 import { type GameState } from '../../game/types';
 
 interface GameSceneProps {
@@ -43,93 +43,38 @@ export default function GameScene({ gameState, myPlayerId }: GameSceneProps) {
                 position: [cam.position.x, cam.position.y, cam.position.z],
                 fov: 50,
                 near: 0.1,
-                far: 100,
+                far: 60,
             }}
-            gl={{ antialias: true, toneMapping: THREE.ACESFilmicToneMapping, toneMappingExposure: 1.5 }}
+            gl={{
+                antialias: true,
+                toneMapping: THREE.ACESFilmicToneMapping,
+                toneMappingExposure: 1.2,
+                outputColorSpace: THREE.SRGBColorSpace
+            }}
             style={{ position: 'absolute', inset: 0 }}
             onCreated={({ camera }) => {
                 camera.lookAt(cam.lookAt);
             }}
         >
-            <ambientLight intensity={1.5} color="#ffd699" />
+            <ambientLight intensity={0.15} color="#ffd699" />
             <spotLight
-                position={[0, 10, 0]}
-                angle={1.2}
-                penumbra={0.5}
-                intensity={15}
+                position={[0, 7, 0]}
+                angle={0.8}
+                penumbra={0.7}
+                intensity={22}
                 color="#fff1d0"
                 castShadow
                 shadow-mapSize-width={2048}
                 shadow-mapSize-height={2048}
+                shadow-bias={-0.0001}
             />
-            <pointLight position={[0, 2, 0]} intensity={3} color="#1a5c3a" distance={15} />
-            <color attach="background" args={['#1a1512']} />
+            <pointLight position={[0, 1.5, 0]} intensity={2.5} color="#1a5c3a" distance={8} />
+            <fog attach="fog" args={['#070503', 8, 28]} />
+            <color attach="background" args={['#070503']} />
 
-            <group>
-                <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.05, 0]} receiveShadow>
-                    <planeGeometry args={[100, 100]} />
-                    <meshStandardMaterial color="#3d2b1f" roughness={1} side={THREE.DoubleSide} />
-                </mesh>
-
-                {/* THE MAIN BACK WALL (North - Visible from Camera) */}
-                <group position={[0, 0, 15]}>
-                    <mesh position={[0, 5, 0]} receiveShadow>
-                        <planeGeometry args={[50, 10]} />
-                        <meshStandardMaterial color="#4d2e1c" roughness={0.5} side={THREE.DoubleSide} />
-                    </mesh>
-                    <group position={[0, 0, -0.2]}>
-                        {[2, 4, 6, 8].map((y) => (
-                            <mesh key={y} position={[0, y, 0]} receiveShadow castShadow>
-                                <boxGeometry args={[30, 0.15, 0.6]} />
-                                <meshStandardMaterial color="#2b1a10" roughness={0.8} />
-                            </mesh>
-                        ))}
-                        <Bottle position={[3, 2.8, 0]} color="#6e5b51" />
-                        <Bottle position={[3.3, 2.8, 0]} color="#2a4d41" type="tall" />
-                        <Bottle position={[3.6, 2.8, 0]} color="#8a5a3a" />
-                        <Book position={[-4, 2.8, 0]} color="#4d2a2a" />
-                        <Book position={[-4.2, 2.82, 0]} color="#2a3b4d" rotation={[0, 0.1, 0]} />
-                        <Bottle position={[-6, 4.8, 0]} color="#6a4a2a" />
-                        <Bottle position={[-5.7, 4.8, 0]} color="#3a5a2a" type="flat" />
-                        <Book position={[5, 4.81, 0]} color="#5a5a2a" />
-                        <Book position={[5.2, 4.82, 0]} color="#2a2a2a" />
-                        <Bottle position={[0, 6.8, 0]} color="#d07024" type="tall" />
-                        <Bottle position={[0.4, 6.8, 0]} color="#2a2a2a" />
-                        <Book position={[-2, 8.8, 0]} color="#333" />
-                    </group>
-                    <WallSconce position={[-12, 5, -0.3]} rotation={[0, Math.PI, 0]} />
-                    <WallSconce position={[12, 5, -0.3]} rotation={[0, Math.PI, 0]} />
-                </group>
-
-                {/* SIDE WALLS */}
-                <group position={[-20, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
-                    <mesh position={[0, 5, 0]} receiveShadow>
-                        <planeGeometry args={[50, 10]} />
-                        <meshStandardMaterial color="#3d2b1f" roughness={0.6} side={THREE.DoubleSide} />
-                    </mesh>
-                </group>
-                <group position={[20, 0, 0]} rotation={[0, -Math.PI / 2, 0]}>
-                    <mesh position={[0, 5, 0]} receiveShadow>
-                        <planeGeometry args={[50, 10]} />
-                        <meshStandardMaterial color="#3d2b1f" roughness={0.6} side={THREE.DoubleSide} />
-                    </mesh>
-                </group>
-
-                {/* FRONT WALL (Behind Camera) */}
-                <group position={[0, 0, -20]}>
-                    <mesh position={[0, 5, 0]} receiveShadow>
-                        <planeGeometry args={[50, 10]} />
-                        <meshStandardMaterial color="#3d2b1f" roughness={0.6} side={THREE.DoubleSide} />
-                    </mesh>
-                </group>
-
-                <mesh position={[0, 10, 0]} rotation={[Math.PI / 2, 0, 0]}>
-                    <planeGeometry args={[100, 100]} />
-                    <meshStandardMaterial color="#111" roughness={1} side={THREE.DoubleSide} />
-                </mesh>
-            </group>
-
+            <SpeakeasyRoom />
             <PokerTable />
+
             {orderedPlayers.map((player, idx) => {
                 const pos = playerPositions[idx];
                 const cardPositions = getCardPositions(player.influenceCards.length, pos.position, pos.rotation);
@@ -156,20 +101,98 @@ export default function GameScene({ gameState, myPlayerId }: GameSceneProps) {
                     </group>
                 );
             })}
-            <group position={[0, 6.5, 0]}>
+
+            {/* Central Ceiling Lamp */}
+            <group position={[0, 6.8, 0]}>
                 <mesh>
-                    <coneGeometry args={[1.5, 0.7, 6, 1, true]} />
-                    <meshStandardMaterial color="#333" roughness={0.8} metalness={0.3} side={THREE.DoubleSide} />
+                    <coneGeometry args={[1.2, 0.6, 6, 1, true]} />
+                    <meshStandardMaterial color="#1a1a1a" roughness={0.7} side={THREE.DoubleSide} />
                 </mesh>
-                <mesh position={[0, -0.2, 0]}>
-                    <circleGeometry args={[1.4, 16]} />
-                    <meshStandardMaterial color="#ffe4b5" emissive="#ffe4b5" emissiveIntensity={1.2} side={THREE.DoubleSide} />
-                </mesh>
-                <mesh position={[0, 2, 0]}>
-                    <cylinderGeometry args={[0.02, 0.02, 4, 4]} />
-                    <meshStandardMaterial color="#222" />
+                <mesh position={[0, -0.25, 0]}>
+                    <circleGeometry args={[1, 16]} />
+                    <meshStandardMaterial color="#ffe4b5" emissive="#ffe4b5" emissiveIntensity={1.5} side={THREE.DoubleSide} />
                 </mesh>
             </group>
         </Canvas>
+    );
+}
+
+function SpeakeasyRoom() {
+    return (
+        <group>
+            {/* INTIMATE FLOOR */}
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.01, 0]} receiveShadow>
+                <planeGeometry args={[25, 25]} />
+                <meshStandardMaterial color="#1a0f05" roughness={0.8} map={woodTex} />
+            </mesh>
+
+            {/* BACK WALL (North - Behind opponents) */}
+            <group position={[0, 0, 7]}>
+                {/* Mahogany Paneling */}
+                <mesh position={[0, 1.5, 0]} receiveShadow>
+                    <planeGeometry args={[20, 3]} />
+                    <meshStandardMaterial color="#2d1a08" roughness={0.3} map={woodTex} side={THREE.DoubleSide} />
+                </mesh>
+                {/* Brick Upper */}
+                <mesh position={[0, 6.5, -0.05]} receiveShadow>
+                    <planeGeometry args={[20, 7]} />
+                    <meshStandardMaterial color="#3d2218" roughness={1} map={brickTex} side={THREE.DoubleSide} />
+                </mesh>
+                {/* Shelves */}
+                <group position={[0, 0, 0.2]}>
+                    {[2, 3.5, 5, 6.5].map((y) => (
+                        <mesh key={y} position={[0, y, 0]} receiveShadow castShadow>
+                            <boxGeometry args={[15, 0.08, 0.5]} />
+                            <meshStandardMaterial color="#1a0f05" roughness={0.6} map={woodTex} />
+                        </mesh>
+                    ))}
+                    {/* Populated Props */}
+                    <Bottle position={[2, 2.3, 0]} color="#4e3b31" />
+                    <Bottle position={[2.2, 2.3, 0]} color="#1a3d31" type="tall" />
+                    <Bottle position={[2.4, 2.3, 0]} color="#7a4a2a" />
+                    <Book position={[-3, 2.3, 0]} color="#3d1a1a" />
+                    <Book position={[-3.1, 2.3, 0]} color="#1a2b3d" rotation={[0, 0.1, 0]} />
+                    <Bottle position={[-5, 3.8, 0]} color="#5a3a1a" />
+                    <Bottle position={[-4.8, 3.8, 0]} color="#2a4a1a" type="flat" />
+                    <Book position={[4, 3.8, 0]} color="#4a4a1a" />
+                    <Book position={[4.1, 3.8, 0]} color="#1a1a1a" />
+                    <Bottle position={[0, 5.3, 0]} color="#c06014" type="tall" />
+                </group>
+                <WallSconce position={[-7, 4, 0.1]} />
+                <WallSconce position={[7, 4, 0.1]} />
+            </group>
+
+            {/* SIDE WALLS - Closer for intimacy */}
+            <group position={[-10, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+                <mesh position={[0, 5, 0]} receiveShadow>
+                    <planeGeometry args={[20, 10]} />
+                    <meshStandardMaterial color="#2d1a08" roughness={0.5} map={woodTex} side={THREE.DoubleSide} />
+                </mesh>
+                <mesh position={[0, 0.8, 0.8]} castShadow receiveShadow>
+                    <boxGeometry args={[8, 1.6, 1.2]} />
+                    <meshStandardMaterial color="#1a0f05" roughness={0.3} map={woodTex} />
+                </mesh>
+                <WallSconce position={[0, 4, 0.1]} />
+            </group>
+
+            <group position={[10, 0, 0]} rotation={[0, -Math.PI / 2, 0]}>
+                <mesh position={[0, 5, 0]} receiveShadow>
+                    <planeGeometry args={[20, 10]} />
+                    <meshStandardMaterial color="#2d1a08" roughness={0.5} map={woodTex} side={THREE.DoubleSide} />
+                </mesh>
+                <group position={[0, 0.8, 1]}>
+                    <Barrel position={[0, 0, 0]} />
+                    <Barrel position={[1, 0, 0.2]} rotation={[0, 0.4, 0]} />
+                    <Barrel position={[0.5, 1, 0]} rotation={[Math.PI / 2, 0, 0]} />
+                </group>
+                <WallSconce position={[0, 4, 0.1]} />
+            </group>
+
+            {/* CEILING */}
+            <mesh position={[0, 10, 0]} rotation={[Math.PI / 2, 0, 0]}>
+                <planeGeometry args={[25, 25]} />
+                <meshStandardMaterial color="#050403" roughness={1} side={THREE.DoubleSide} />
+            </mesh>
+        </group>
     );
 }
--- Coup/src/components/game3d/Props.tsx
+++ Coup/src/components/game3d/Props.tsx
@@ -1,13 +1,65 @@
 import * as THREE from 'three';
 
+// Procedural Wood Grain Texture
+function getWoodTexture() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 128;
+    canvas.height = 128;
+    const ctx = canvas.getContext('2d')!;
+    ctx.fillStyle = '#2a1a05';
+    ctx.fillRect(0, 0, 128, 128);
+    ctx.strokeStyle = '#3d2b1f';
+    for (let i = 0; i < 40; i++) {
+        ctx.beginPath();
+        const y = Math.random() * 128;
+        ctx.moveTo(0, y);
+        ctx.bezierCurveTo(40, y + 10, 80, y - 10, 128, y);
+        ctx.stroke();
+    }
+    const tex = new THREE.CanvasTexture(canvas);
+    tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+    return tex;
+}
+
+// Procedural Brick Texture
+function getBrickTexture() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 256;
+    canvas.height = 256;
+    const ctx = canvas.getContext('2d')!;
+    ctx.fillStyle = '#3d2218';
+    ctx.fillRect(0, 0, 256, 256);
+    ctx.strokeStyle = '#222';
+    ctx.lineWidth = 2;
+    for (let y = 0; y < 256; y += 32) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(256, y);
+        ctx.stroke();
+        const offset = (y / 32) % 2 === 0 ? 0 : 32;
+        for (let x = offset; x < 256; x += 64) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + 32);
+            ctx.stroke();
+        }
+    }
+    const tex = new THREE.CanvasTexture(canvas);
+    tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+    return tex;
+}
+
+const woodTex = getWoodTexture();
+const brickTex = getBrickTexture();
+
 export function Bottle({ position, type = 'classic', color = '#c06014' }: { position: [number, number, number], type?: 'classic' | 'tall' | 'flat', color?: string }) {
     return (
         <group position={position}>
             <mesh castShadow>
-                {type === 'classic' && <cylinderGeometry args={[0.08, 0.08, 0.3, 8]} />}
-                {type === 'tall' && <cylinderGeometry args={[0.06, 0.06, 0.45, 8]} />}
+                {type === 'classic' && <cylinderGeometry args={[0.08, 0.08, 0.3, 12]} />}
+                {type === 'tall' && <cylinderGeometry args={[0.06, 0.06, 0.45, 12]} />}
                 {type === 'flat' && <boxGeometry args={[0.15, 0.3, 0.06]} />}
-                <meshStandardMaterial color={color} transparent opacity={0.7} roughness={0.1} metalness={0.2} />
+                <meshStandardMaterial color={color} transparent opacity={0.6} roughness={0.05} metalness={0.5} />
             </mesh>
             <mesh position={[0, type === 'tall' ? 0.25 : 0.2, 0]} castShadow>
                 <cylinderGeometry args={[0.02, 0.02, 0.1, 8]} />
@@ -40,8 +92,8 @@ export function Barrel({ position, rotation = [0, 0, 0] }: { position: [number, 
     return (
         <group position={position} rotation={rotation}>
             <mesh castShadow>
-                <cylinderGeometry args={[0.4, 0.5, 1.2, 12]} />
-                <meshStandardMaterial color="#3d2b1f" roughness={0.9} />
+                <cylinderGeometry args={[0.4, 0.5, 1.2, 16]} />
+                <meshStandardMaterial color="#3d2b1f" roughness={0.9} map={woodTex} />
             </mesh>
             {[0.3, -0.3, 0.5, -0.5].map((y, i) => (
                 <mesh key={i} position={[0, y, 0]}>
@@ -58,29 +110,31 @@ export function WallSconce({ position, rotation = [0, 0, 0] }: { position: [numb
         <group position={position} rotation={rotation}>
             <mesh castShadow>
                 <boxGeometry args={[0.1, 0.4, 0.05]} />
-                <meshStandardMaterial color="#2a1a05" metalness={0.6} roughness={0.3} />
+                <meshStandardMaterial color="#1a0f05" metalness={0.6} roughness={0.3} />
             </mesh>
             <mesh position={[0, 0.1, 0.15]}>
-                <boxGeometry args={[0.05, 0.05, 0.3]} />
-                <meshStandardMaterial color="#2a1a05" metalness={0.6} roughness={0.3} />
+                <boxGeometry args={[0.04, 0.04, 0.3]} />
+                <meshStandardMaterial color="#1a0f05" metalness={0.6} roughness={0.3} />
             </mesh>
             <mesh position={[0, 0.2, 0.25]}>
-                <sphereGeometry args={[0.1, 8, 8]} />
+                <sphereGeometry args={[0.12, 12, 12]} />
                 <meshStandardMaterial
                     color="#ffd27d"
                     emissive="#ff8c00"
-                    emissiveIntensity={2.5}
+                    emissiveIntensity={4}
                     transparent
-                    opacity={0.8}
+                    opacity={0.7}
                 />
             </mesh>
             <pointLight
                 position={[0, 0.2, 0.4]}
-                intensity={1.5}
+                intensity={2.5}
                 color="#ff8c00"
-                distance={12}
-                decay={1.5}
+                distance={6}
+                decay={2}
             />
         </group>
     );
 }
+
+export { woodTex, brickTex };
